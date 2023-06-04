dac97b0edcc7ed1aa34f106afc17b50b52147501
PrakharDsek <105925167+PrakharDsek@users.noreply.github.com>
2023-06-04 09:46:45 +0530

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import Store from "../Redux/Store.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
       <Router>
